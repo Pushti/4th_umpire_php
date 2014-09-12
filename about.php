615358561ef1eff185277cd1b25d8b4f8d97683c
unknown <Pushti>
2014-09-12 16:00:33 -0700

    about page of fourth umpire

---
+++ about.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>4thUmpire - About Us</title>
+<?php
+	include 'include/css.html';
+?>
+
+</head>
+
+<body>
+
+                           
+<div id="sb-site">
+  <div class="boxed">
+   <!---- Header Start ---->
+   <?php 
+   		include 'include/header.html';
+   ?>
+  <!---- Header Ends ---->
+    
+    <!---- Menu Start ---->
+	<?php
+		include 'include/menu.html';
+	?>
+   <!---- Menu Ends ---->  
+   
+    <header class="main-header">
+            <div class="container">
+                <h1 class="page-title">About us </h1>
+
+                <ol class="breadcrumb pull-right">
+                    <li><a href="index.php">Home</a></li>
+                    <li class="active">About Us</li>
+                </ol>
+            </div>
+        </header>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="title-logo animated fadeInDown animation-delay-5"><img src="images/Chennai_Super_Kings_Logo.png" /></div>
+                </div>
+                <div class="col-xs-12">
+                    <p class="slogan text-center animated bounceInLeft animation-delay-12">The <span>Chennai Super Kings</span> are a franchise cricket team based in Chennai, Tamil Nadu that plays in the Indian Premier League. Founded in 2008, the team is <span>captained by Mahendra Singh Dhoni</span> and <span>coached by Stephen Fleming</span>.</p>
+                </div>
+                <div class="col-md-8 wow fadeInUp">
+                    <h2 class="right-line">About us</h2>
+                    <p class="p-lg">The Chennai Super Kings (abbreviated as CSK) are a franchise cricket team based in Chennai, Tamil Nadu that plays in the Indian Premier League. Founded in 2008, the team is captained by Mahendra Singh Dhoni and coached by Stephen Fleming. The team plays its home matches at the M. A. Chidambaram Stadium in Chennai.</p>
+                    <p class="p-lg">The Super Kings are the most successful team in the IPL[1] and have won the title twice in succession (2010 and 2011).[2] In 2010, they became the first Indian team to win the Champions League Twenty20. The leading run-scorer of the team is Suresh Raina, while the leading wicket-taker is Ravichandran Ashwin.[3][4] The brand value of the Super Kings in 2012 was estimated at US$75.13 million, making them the most valuable franchise in the IPL.</p>
+                    <hr class="dotted">
+                    <blockquote class="blockquote-color-bg-primary">
+                        <p>The Chennai Super Kings are a franchise cricket team based in Chennai, Tamil Nadu that plays in the Indian Premier League. Founded in 2008, the team is captained by Mahendra Singh Dhoni and coached by Stephen Fleming.</p>
+                        <footer>captained by Mahendra Singh Dhoni</footer>
+                    </blockquote>
+                </div>
+                <div class="col-md-4 wow fadeInUp">
+                    <h2 class="right-line">Player List</h2>
+                    <div class="row">
+                        <ul class="plyerlist_about">
+                        	<li><a href="#">MS Dhoni</a></li>
+                            <li><a href="#">Suresh Raina </a></li>
+                            <li><a href="#">Ravindra Jadeja </a></li>
+                            <li><a href="#">R. Ashwin </a></li>
+                            <li><a href="#">Mohit Sharma</a></li>
+                            <li><a href="#">Ashish Nehra</a></li>
+                            <li><a href="#">Mithun Manhas</a></li>
+                            <li><a href="#">Baba Aparajith </a></li>
+                            <li><a href="#">Ishwar Pandey </a></li>
+                            <li><a href="#">Vijay Shankar</a></li>
+                            <li><a href="#">Ronit More </a></li>
+                            <li><a href="#">Pawan Negi </a></li>
+                            <li><a href="#">Dwayne Bravo </a></li>
+                            <li><a href="#">Faf du Plessis</a></li>
+                            <li><a href="#">Dwayne Smith </a></li>
+                            <li><a href="#">BB McCullum </a></li>
+                            <li><a href="#">Ben Hilfenhaus </a></li>
+                            <li><a href="#">Samuel Badree </a></li>
+                            <li><a href="#">Matt Henry </a></li>
+                            <li><a href="#">John Hastings </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div>
+        
+        
+     <!---- Footer Start ---->
+   		<?php
+			include 'include/footer.html';
+		?>
+   <!---- Footer Ends ---->
+  </div>
+  <!-- boxed --> 
+</div>   
+    
+	
+	
+<script src="js/fourth_umpire.js"></script> 
+
+
+</body>
+</html>
